d1d43e73b0f7cc9a30b190b5396e5023bcbef6c4
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2023-10-13 13:10:14 -0400

    Getting the checking of adjacent tiles done.

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -5,17 +5,21 @@
 struct Hex {
   int x, y;
   bool blocked;
+  bool rightShifted;
+  bool weight;
   
-  void setHex(int x, int y, bool blocked)
+  void setHex(int x, int y, bool blocked, bool shifted)
   {
 	  this->x = x;
 	  this->y = y;
 	  this->blocked = blocked;
+      this->rightShifted = shifted;
   }
   Hex(){ 
 	  x = 0;
           y = 0;
           blocked = false;
+          rightShifted = false;
   };
 };
 
@@ -31,31 +35,241 @@ struct Cat : public IAgent {
 		int minSide;
         int maxSide;
 
+		Hex catHex;
+
+		//Figuring out how wide and tall the grid is with its negative to positive form.
 		if (sideSize % 2 == 0)
 		{
-          minSide = (-sideSize / 2) - 1;
-                  maxSide = sideSize / 2;
+			minSide = (-sideSize / 2) - 1;
+            maxSide = sideSize / 2;
 		}
 		else
 		{
-                  minSide = -sideSize / 2;
-                  maxSide = sideSize / 2;
+            minSide = -sideSize / 2;
+            maxSide = sideSize / 2;
 		}
 
+		//Getting the positions into an easily understandable format.
 		int dataPull = 0;
+        bool shift = false;
 		for (int i = minSide; i < maxSide; i++)
 		{
-                
-					for (int j = minSide; j < maxSide; j++)
-					{
+				for (int j = minSide; j < maxSide; j++)
+				{
                     Hex aHex;
-                    aHex.setHex(i, j, world.at(dataPull));
-                    grid.push_back(aHex);
+					if (i % 2 != 0)
+					{
+						bool shift = true;
+                    } 
+					else
+					{
+						shift = false;
+                    }
+                    aHex.setHex(i, j, world.at(dataPull), shift);
+                    
+					if (i == catPos.first && j == catPos.second)
+					{
+                        aHex.weight = 1000;
+						catHex = aHex;
+                        grid.push_back(aHex);
+                    }
+					else
+					{
+                        grid.push_back(aHex);
+                    }
                     dataPull++;
-					}
+				}
 		}
+		
+		Hex currentHex = catHex;
+        currentHex.weight = 0;
+		//Checking adjecent tiles.
+		bool exitFound = false;
+		while (!exitFound)
+		{
+			for (int i = 0; i < grid.size(); i++)
+			{
+				int tileX = grid.at(i).x;
+				int tileY = grid.at(i).y;
+
+				if (!currentHex.rightShifted)
+				{
+					if (currentHex.x - 1 == tileX && currentHex.y - 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x == tileX && currentHex.y - 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x - 1 == tileX && currentHex.y == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x + 1 == tileX && currentHex.y == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x - 1 == tileX && currentHex.y + 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x == tileX && currentHex.y + 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (currentHex.x == tileX && currentHex.y - 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x + 1 == tileX && currentHex.y - 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x - 1 == tileX && currentHex.y == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x + 1 == tileX && currentHex.y == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x == tileX && currentHex.y + 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+					if (currentHex.x + 1 == tileX && currentHex.y + 1 == tileY)
+					{
+						if (grid.at(i).weight == 0)
+						{
+							if (grid.at(i).blocked)
+							{
+								grid.at(i).weight = 1000;
+							}
+							else
+							{
+								grid.at(i).weight = currentHex.weight + 1;
+							}
+						}
+					}
+				}
 
 
+			}
+		}
 		
 		return {xMove,yMove}; // todo: change this
 	}
